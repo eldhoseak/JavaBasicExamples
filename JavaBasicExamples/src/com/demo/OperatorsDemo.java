a6de5fc928532e000d15e5f134c2a05774690d50
Eldhose <eldhose.ak@gmail.com>
2019-10-19 07:10:54 +0530

    initial commit

---
+++ JavaBasicExamples/src/com/demo/OperatorsDemo.java
@@ -0,0 +1,40 @@
+package com.demo;
+
+public class OperatorsDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		long longVal = 100l;
+		
+		// type casting examples
+		int val1 = (int) longVal;
+		
+		int val2 = 50;
+		System.out.println("val1 = "+val1+ ", val2 = "+val2);
+		
+		System.out.println("------  Arithmetic operators"+" START-------");
+		System.out.println("val1+val2 = "+ (val1+val2));
+		System.out.println("val1-val2 = "+ (val1-val2));
+		System.out.println("val1*val2 = "+ (val1*val2));
+		System.out.println("val1/val2 = "+ (val1/val2));
+		System.out.println("val1%val2 = "+ (val1%val2));
+		System.out.println("------  Arithmetic operators END-------");
+		
+		System.out.println("------  Relational operators START-------");
+		System.out.println("val1>val2 = "+ (val1>val2));
+		System.out.println("val1<val2 = " + (val1<val2));
+		System.out.println("val1>=val2 = " + (val1>=val2));
+		System.out.println("val1<=val2 = " + (val1<=val2));
+		System.out.println("val1!=val2 = " + (val1!=val2));
+		System.out.println("val1==val2 = " + (val1==val2));
+		System.out.println("------  Relational operators END-------");
+		
+		System.out.println("------  Logical operators START-------");
+		System.out.println("val1>val2 && val1<val2 = "+ (val1>val2 && val1<val2));
+		System.out.println("val1>val2 || val1<val2 = "+ (val1>val2 || val1<val2));
+		System.out.println("------  Logical operators END-------");
+
+	}
+
+}
