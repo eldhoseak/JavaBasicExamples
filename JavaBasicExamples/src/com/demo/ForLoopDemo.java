b19e6de2c7cb192d1ada89f61dc8c0aa6043f9f8
Eldhose <eldhose.ak@gmail.com>
2019-10-23 07:11:51 +0530

    updated examples for if and for

---
+++ JavaBasicExamples/src/com/demo/ForLoopDemo.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+public class ForLoopDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int sum = 0;	
+for(int i=1; i<=10; i++){
+	
+	sum = sum +i;
+	System.out.println(i);
+}
+
+System.out.println("sum ="+sum);
+	}
+
+}
---
+++ JavaBasicExamples/src/com/demo/IfConditionDemo.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+public class IfConditionDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int age =55;	
+		if(age>60){
+		System.out.println("senior citizen");	
+		}else if(age>50){
+			System.out.println("not so young citizen");
+		}else{
+			System.out.println("young citizen");
+		}
+	}
+
+}
