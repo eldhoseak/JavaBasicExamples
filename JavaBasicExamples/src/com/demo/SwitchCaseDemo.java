f39f46237752e7e8ef635d40cbe2c573526d2ddd
Eldhose <eldhose.ak@gmail.com>
2019-10-25 07:05:29 +0530

    Loop demo

---
+++ JavaBasicExamples/src/com/demo/SwitchCaseDemo.java
@@ -0,0 +1,38 @@
+package com.demo;
+
+public class SwitchCaseDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int number =1 ;
+		
+		switch(number){
+		case 1:
+			{
+				System.out.println("ONE");
+			break;
+			}
+		case 2:
+		{
+			System.out.println("TWO");
+			break;	
+		}
+			
+		case 3:
+		{
+			System.out.println("THREE");
+			break;
+		}
+		
+		default:
+		{
+			System.out.println("NOT MAPPED");
+		}
+		
+		}
+		
+
+	}
+
+}
---
+++ JavaBasicExamples/src/com/demo/WhileLoopDemo.java
@@ -0,0 +1,27 @@
+package com.demo;
+
+public class WhileLoopDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int count =15;
+		
+		while(count <= 10){
+			System.out.println(count);
+			if(count == 5){
+				continue;
+			}
+			count++;
+			
+		}
+		
+		
+	/*	do{
+			System.out.println(count);
+			count++;
+			
+		}while(count <= 10);
+*/
+	}
+
+}
