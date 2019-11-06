4fcf2039116d08b414ec325007e18cf9f10cee5f
Eldhose <eldhose.ak@gmail.com>
2019-11-06 07:44:49 +0530

    methods in java

---
+++ JavaBasicExamples/src/com/demo/MethodExample.java
@@ -0,0 +1,30 @@
+package com.demo;
+
+public class MethodExample {
+
+	static int  calculateSum (int num1, int num2){
+		return num1 + num2;
+	}
+	
+	
+	static double  calculateSum (double num1, double num2){
+		System.out.println("inside double");
+		return num1 + num2;
+		
+	}
+	
+static float  calculateSum (float num1, float num2){
+		System.out.println("inside float");
+		return num1 + num2;
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(calculateSum(10,5));
+System.out.println(calculateSum(10.5,10.25));
+System.out.println(calculateSum(20.1f,10.5f));
+	}
+
+}
