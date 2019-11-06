20773f4f30912c82d3ad78c66d51842ff29a34b7
GurkaranLally <57423573+GurkaranLally@users.noreply.github.com>
2019-11-06 16:47:14 -0500

    Gurkaran

--- JavaBasicExamples/src/com/demo/MethodExample.java
+++ JavaBasicExamples/src/com/demo/MethodExample.java
@@ -3,19 +3,20 @@ package com.demo;
 public class MethodExample {
 
 	static int  calculateSum (int num1, int num2){
-		return num1 + num2;
+		System.out.println("int");	
+		return num1 - num2;
 	}
 	
 	
 	static double  calculateSum (double num1, double num2){
-		System.out.println("inside double");
-		return num1 + num2;
+		System.out.println("double");
+		return num1 * num2;
 		
 	}
 	
 static float  calculateSum (float num1, float num2){
-		System.out.println("inside float");
-		return num1 + num2;
+		System.out.println("float");
+		return num1 / num2;
 		
 	}
 	
@@ -27,4 +28,4 @@ System.out.println(calculateSum(10.5,10.25));
 System.out.println(calculateSum(20.1f,10.5f));
 	}
 
-}
+(
