250975d295360c93cf0a43d1679409525d88d045
Eldhose <eldhose.ak@gmail.com>
2019-11-08 08:22:20 +0530

    Array Demo

---
+++ JavaBasicExamples/src/com/demo/ArrayDemo.java
@@ -0,0 +1,32 @@
+package com.demo;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		// Length of the longest increasing subsequence. below it is 4 -> 2,5,7,8
+
+		int length = 0, counter =0;
+		int [] numericArray = {1, 4, 7, 3, 2,5, 7 ,8};
+
+		for (int i= 0; i<numericArray.length;i++){
+			
+			for (int j= i+1; j<numericArray.length;j++){
+				if(numericArray[j]> numericArray[j-1]){
+					counter ++;
+				}
+			}
+			
+			if(length< counter){
+				length = counter;
+			}
+			counter = 0;
+		}
+
+		System.out.println("length "+ length);
+
+	}
+
+}
