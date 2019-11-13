e529e13100703c2dff943e009b73015d5ad8e652
Eldhose <eldhose.ak@gmail.com>
2019-11-13 07:35:18 +0530

    updated

--- JavaBasicExamples/src/com/demo/PolicyHolder.java
+++ JavaBasicExamples/src/com/demo/PolicyHolder.java
@@ -1,9 +1,21 @@
 package com.demo;
 
 public class PolicyHolder{
+	
 	private int policyNo;
 	private double bonus;
 
+	public PolicyHolder(){
+		this.bonus =400.0;
+		this.policyNo = 1001;
+	}
+	
+	public PolicyHolder(int policyNo, double bonus){
+		this.bonus =bonus;
+		this.policyNo = policyNo;
+	}
+	
+	
 	public void setPolicyNo(int no){policyNo = no;}
 	public int getPolicyNo(){return policyNo;}
 	public void setBonus(double amount){
@@ -11,4 +23,8 @@ public class PolicyHolder{
 			bonus = amount;}}
 	public double getBonus(){return bonus;}
 
+	public boolean isSame(PolicyHolder policyHolder){
+		return this.policyNo == policyHolder.policyNo;
+	}
+	
 }
--- JavaBasicExamples/src/com/demo/PolicyHolderTest.java
+++ JavaBasicExamples/src/com/demo/PolicyHolderTest.java
@@ -5,13 +5,22 @@ public class PolicyHolderTest {
 	
 	
 	public static void main(String[] args) {
-		 PolicyHolder policyHolder = new PolicyHolder();
-		policyHolder.setPolicyNo(131030);
-		policyHolder.setBonus(-1);
-
+		 PolicyHolder policyHolder = new PolicyHolder(600, 500);
+		 
+		 PolicyHolder policyHolder2 =getInstance();
+		
 		System.out.println(policyHolder.getBonus());
 		System.out.println(policyHolder.getPolicyNo());
 		
+		System.out.println(policyHolder2.getBonus());
+		System.out.println(policyHolder2.getPolicyNo());
+		
+		
+		System.out.println(policyHolder.isSame(policyHolder2));
+	}
+	
+	private static PolicyHolder getInstance(){
+		return new PolicyHolder();
 	}
 
 }
