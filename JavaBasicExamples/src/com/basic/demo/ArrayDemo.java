8eb4b223b8c124753858eed04996de9f4ec7b857
eldhose.abraham <eldhose.abraham@ABRAHAM-ITG.MMTCPAMP.COM>
2019-11-15 11:05:51 +0530

    Fixed compilation issues.

--- JavaBasicExamples/src/com/basic/demo/ArrayDemo.java
+++ JavaBasicExamples/src/com/basic/demo/ArrayDemo.java
@@ -1,7 +1,7 @@
 package com.basic.demo;
 
 public class ArrayDemo {
-/*
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
@@ -29,34 +29,7 @@ public class ArrayDemo {
 
 	}
 }
-package com.demo;
-					*/
-public class ArrayAssignment {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-		int length = 0,	counter =0;
-		int [] numericArray = {1, 4, 7, 3, 2, 5, 7 ,8};
-		
-		for (int i=1; i<numericArray.length; i++) {
-			
-			for (int j =i+1; j<numericArray.length; j++) {
-				if(numericArray[j] > numericArray[j-1]) {
-					counter ++;}
-		
-		}
-			
-		if (length< counter) {
-			length =  counter;
-		
-		}	
-		counter = 0;
-		}
-		
-	System.out.println("Length" + length);
-
-		}
-	}
 
 	
--- JavaBasicExamples/src/com/basic/demo/MethodExample.java
+++ JavaBasicExamples/src/com/basic/demo/MethodExample.java
@@ -4,19 +4,19 @@ public class MethodExample {
 
 	static int  calculateSum (int num1, int num2){
 		System.out.println("int");	
-		return num1 - num2;
+		return num1 + num2;
 	}
 	
 	
 	static double  calculateSum (double num1, double num2){
 		System.out.println("double");
-		return num1 * num2;
+		return num1 + num2;
 		
 	}
 	
 static float  calculateSum (float num1, float num2){
 		System.out.println("float");
-		return num1 / num2;
+		return num1 + num2;
 		
 	}
 	
@@ -27,5 +27,6 @@ System.out.println(calculateSum(10,5));
 System.out.println(calculateSum(10.5,10.25));
 System.out.println(calculateSum(20.1f,10.5f));
 	}
+}
 
-(
+
